1e4ec678f17ae07f9b542ee95f5f4ec034291f24
hamid mujtaba <hamipirzada@gmail.com>
2025-06-19 16:20:34 +0530

    Update application.py

--- application.py
+++ application.py
@@ -195,4 +195,5 @@ def test_odoo():
 
 # For local testing
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
